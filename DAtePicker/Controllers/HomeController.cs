364f75095b836d86d3dc5293e71c2c9432709dd0
Mac Beath <mac.beath@hotmail.com>
2019-01-09 21:31:32 -0500

    IMportingh Project

---
+++ DAtePicker/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using DAtePicker.Models;
+using System.Web.Mvc;
+
+namespace DAtePicker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Person p = new Person();
+          
+            return View(p);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DAtePicker/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAtePicker.Models
+{
+    public class Person
+    {
+        
+        public int ID { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
